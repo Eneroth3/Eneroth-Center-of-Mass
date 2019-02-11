2779e74f5f8b4fe122a89cbd0ffcf81d32ba9c19
Eneroth3 <eneroth3@gmail.com>
2019-02-11 15:31:51 +0100

    Seemingly working proof of concept

--- center of gravity.rb
+++ center of gravity.rb
@@ -3,37 +3,62 @@ def self.center_of_grafity(entities = Sketchup.active_model.selection)
   # the meshes and an arbitrarily selected "tip" point. Sum the volumes of the
   # tetrahedrons corresponding to faces facing away from the tip, and subtract
   # the volume of those facing towards it (forming a cavity).
-  
-  point = Geom::Point3d.new
+
+  center = Geom::Point3d.new
   volume = 0
-  # TODO: Use center of entities to reduce deviations due to floating point
-  # precision.
+  # TODO: Use approximate center of entities to reduce deviations due to
+  # floating point precision (typically bounding box center).
   tip_point = ORIGIN
-  
-  # TEST CODE
-  Sketchup.active_model.start_operation("TEST")
-  
+
+  # TODO: Separate recursive loop of instances from loop of faces inside them
+  # and calculate the volume for each instance separately, so it can be
+  # multiplied by its density.
   traverse_entities(entities) do |face, transformation|
     next unless face.is_a?(Sketchup::Face)
-    
     triangles = triangulate_face(face, transformation)
-
-    # TEST CODE
-    triangles.each { |t| Sketchup.active_model.active_entities.add_face(t) }
+    tetrahedrons = triangles.map { |t| t.push(tip_point) }
+    tetrahedrons.each do |tetrahedron|
+      tetra_volume = tetrahedron_volume(tetrahedron)
+      # TODO: Negate local volume when transformation is flipped.
+      volume += tetra_volume
+      tetra_center = tetrahedron_center(tetrahedron)
+      center = add_point(center, multiply_point(tetra_center, tetra_volume))
+    end
   end
-  
-  # TEST CODE
-  Sketchup.active_model.commit_operation
- 
-  divide_point(point, volume)
+
+  divide_point(center, volume)
 end
 
+# Calculate volume of tetrahedron.
+# TODO: Assure volume is negative when the tip point is in the direction of the
+# normal of the opposite face.
+def tetrahedron_volume(tetrahedron)
+  a, b, c, d = tetrahedron
+
+  ((a - d) % ((b - d) * (c - d))) / 6
+end
+
+def tetrahedron_center(tetrahedron)
+  a, b, c, d = tetrahedron
+
+  Geom::Point3d.new(
+    (a.x + b.x + c.x + d.x ) / 4,
+    (a.y + b.y + c.y + d.y ) / 4,
+    (a.z + b.z + c.z + d.z ) / 4,
+  )
+end
+
+# TODO: Extend Point3d with +, +=, *, *=, / and /=.
 def divide_point(point, denominator)
   Geom::Point3d.new(point.to_a.map { |c| c / denominator } )
 end
 
 def multiply_point(point, factor)
   Geom::Point3d.new(point.to_a.map { |c| c * factor } )
+end
+
+def add_point(point1, point2)
+  Geom::Point3d.new(point1.x + point2.x, point1.y + point2.y, point1.z + point2.z)
 end
 
 # Traverse entities recursively.
@@ -80,6 +105,6 @@ def self.triangulate_face(face, transformation = IDENTITY)
   incides = mesh.polygons.flatten.map(&:abs)
   points = incides.map { |i| mesh.point_at(i) }
   points.each { |pt| pt.transform!(transformation) }
-  
+
   points.each_slice(3).to_a
 end
